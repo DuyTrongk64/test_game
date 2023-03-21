fa26ffbcdf8a55122f8876ebca9a06955f14e411
Duytrongk64 <75691078+DuyTrongk64@users.noreply.github.com>
2023-03-22 01:17:29 +0700

    update ship can run

--- assets/scripts/player.ts
+++ assets/scripts/player.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, Vec2, systemEvent, SystemEventType, KeyCode } from 'cc';
+import { _decorator, Component, Node, Vec2,Vec3, systemEvent,SystemEvent, SystemEventType, KeyCode, EventKeyboard, v2, tween, v3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('player')
@@ -7,19 +7,36 @@ export class player extends Component {
     private _movementSpeed = 0;
     private _curPos = new Vec2();
     private _targetPos = new Vec2();
+    private speed: number = 20;
+
     
     start() {
-        systemEvent.on(SystemEventType.KEY_DOWN,this.keyPress(),this)
+        systemEvent.on(SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
     }
 
-    keyPress(event: KeyboardEvent){
-        switch(event.keyCode){
-            case KeyCode.KEY_W:
-                moveBy(50,50);
+    onDestroy() {
+        systemEvent.off(SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    }
+    
+    onKeyDown(event: EventKeyboard) {
+        switch (event.keyCode) {
+            case KeyCode.KEY_A:
+                tween(this.node).by(0.1,{ position: new Vec3(-this.speed, 0, 0) }).start();
                 break;
-            default:  `none`;
+            case KeyCode.KEY_D:
+                tween(this.node).by(0.1,{ position: new Vec3(this.speed, 0, 0)}).start();
+                break;
+            case KeyCode.KEY_W:
+                tween(this.node).by(0.1,{ position: new Vec3(0, this.speed, 0)}).start();
+                break;
+            case KeyCode.KEY_S:
+                tween(this.node).by(0.1,{ position: new Vec3(0, -this.speed, 0)}).start();
+                break;
+            default:
+                break;
         }
     }
+
     update(deltaTime: number) {
         
     }
